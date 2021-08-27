38f4b2d3dcedeaef3d517237ce6acd66bcfa8ad3
mucahitgoktas <mucahitgoktas@gmail.com>
2021-08-27 19:04:13 +0300

    Initial commit

---
+++ Controller/CacheController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RedisDemo.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDatabase _database;
+
+        public CacheController(IDatabase database)
+        {
+            _database = database;
+        }
+
+        // GET api/<CacheController>/5
+        [HttpGet]
+        public string Get([FromQuery]string key)
+        {
+            return _database.StringGet(key);
+        }
+
+        // POST api/<CacheController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        
+    }
+}
